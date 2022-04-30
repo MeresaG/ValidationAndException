6710af524d33cc68113556be7beb318e12e9e7fb
Meresa <meresa27@gmail.com>
2022-04-30 14:57:31 -0500

    initial set up

---
+++ src/test/java/edu/miu/validationAndexception/ValidationAndexceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.validationAndexception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationAndexceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
